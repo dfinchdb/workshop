fb3cb807fd350803a0fd6a4d4ceffa566922e1b7
David Finch <david.finch@databricks.com>
2024-01-13 02:18:13 -0500

    add spark sql method

---
+++ manage_uc_objects/spark_sql/create_uc_objects.py
@@ -0,0 +1,114 @@
+import ast
+import configparser
+import pathlib
+import sys
+
+from pyspark.sql import SparkSession
+
+
+def get_sparksession():
+    return SparkSession.builder.getOrCreate()
+
+
+def get_dbutils(spark: SparkSession):
+    dbutils = None
+
+    if spark.conf.get("spark.databricks.service.client.enabled") == "true":
+        from pyspark.dbutils import DBUtils
+
+        dbutils = DBUtils(spark)
+
+    else:
+        import IPython
+
+        dbutils = IPython.get_ipython().user_ns["dbutils"]
+
+    return dbutils
+
+
+def create_catalog(spark, catalog_name):
+    spark.sql(f"CREATE CATALOG IF NOT EXISTS {catalog_name}")
+
+
+def create_schema(spark, catalog_name, schema_name):
+    spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.{schema_name}")
+
+
+def create_volume(spark, catalog_name, schema_name, volume_name):
+    spark.sql(f"CREATE VOLUME IF NOT EXISTS {catalog_name}.{schema_name}.{volume_name}")
+
+
+def create_external_location(
+    spark, external_location_name, external_url, storage_credential_name, comment=None
+):
+    spark.sql(
+        f"""
+        CREATE EXTERNAL LOCATION IF NOT EXISTS `{external_location_name}` URL '{external_url}'
+            WITH (CREDENTIAL `{storage_credential_name}`)
+            COMMENT '{comment}'
+        """
+    )
+
+
+def create_sample_table(spark, catalog_name, schema_name, table_name):
+    spark.sql(
+        f"CREATE TABLE IF NOT EXISTS {catalog_name}.{schema_name}.{table_name} (id INT, name STRING)"
+    )
+    spark.sql(
+        f"INSERT INTO {catalog_name}.{schema_name}.{table_name} VALUES (1, 'Alice'), (2, 'Bob'), (3, 'Charlie')"
+    )
+
+
+if __name__ == "__main__":
+    spark = get_sparksession()
+    dbutils = get_dbutils(spark)
+
+    catalogs = [{"catalog": "df_test_catalog"}]
+    schemas = [{"catalog": "df_test_catalog", "schema": "df_test_schema"}]
+    volumes = [
+        {
+            "catalog": "df_test_catalog",
+            "schema": "df_test_schema",
+            "volume": "test_volume",
+        }
+    ]
+    tables = [
+        {
+            "catalog": "df_test_catalog",
+            "schema": "df_test_schema",
+            "table": "sample_table",
+        }
+    ]
+    locations = [
+        {
+            "name": "df_test_location",
+            "location": "abfss://umpquapocprd@oneenvadls.dfs.core.windows.net/umpqua_poc/playground",
+            "credential": "121ccfbf-3d4f-4744-87f5-36b1c921c903-storage-credential-1703096015101",
+            "comment": "",
+        }
+    ]
+
+    for catalog in catalogs:
+        create_catalog(spark, catalog.get("catalog"))
+
+    for schema in schemas:
+        create_schema(spark, schema.get("catalog"), schema.get("schema"))
+
+    for volume in volumes:
+        create_volume(
+            spark, volume.get("catalog"), volume.get("schema"), volume.get("volume")
+        )
+
+    for table in tables:
+        create_sample_table(
+            spark, table.get("catalog"), table.get("schema"), table.get("table")
+        )
+
+    for location in locations:
+        create_external_location(
+            spark,
+            location.get("name"),
+            location.get("location"),
+            location.get("credential"),
+            location.get("comment"),
+        )
---
+++ manage_uc_objects/spark_sql/remove_uc_objects.py
@@ -0,0 +1,152 @@
+import ast
+import configparser
+import pathlib
+import sys
+
+from pyspark.sql import SparkSession
+
+
+def get_sparksession():
+    return SparkSession.builder.getOrCreate()
+
+
+def get_dbutils(spark: SparkSession):
+    dbutils = None
+
+    if spark.conf.get("spark.databricks.service.client.enabled") == "true":
+        from pyspark.dbutils import DBUtils
+
+        dbutils = DBUtils(spark)
+
+    else:
+        import IPython
+
+        dbutils = IPython.get_ipython().user_ns["dbutils"]
+
+    return dbutils
+
+
+def drop_tables(spark, catalog_name, schema_name, table_names):
+    if len(table_names) > 0:
+        print(f"Found Tables In {catalog_name}.{schema_name}: {table_names}")
+
+        for table_name in table_names:
+            try:
+                spark.sql(f"DROP TABLE {catalog_name}.{schema_name}.{table_name}")
+                print(f"Dropped Table: {catalog_name}.{schema_name}.{table_name}")
+            except:
+                print(
+                    f"Unable to drop Table: {catalog_name}.{schema_name}.{table_name}"
+                )
+
+
+def drop_volumes(spark, dbutils, catalog_name, schema_name, volume_names):
+    if len(volume_names) > 0:
+        print(f"Found Volumes In {catalog_name}.{schema_name}: {volume_names}")
+
+        for volume_name in volume_names:
+            try:
+                dbutils.fs.rm(
+                    f"/Volumes/{catalog_name}/{schema_name}/{volume_name}/",
+                    recurse=True,
+                )
+                print(
+                    f"Deleted the root directory of Volume: {catalog_name}.{schema_name}.{volume_name}"
+                )
+            except:
+                print(
+                    f"Unable to delete Volume root: /Volumes/{catalog_name}/{schema_name}/{volume_name}/"
+                )
+
+            try:
+                spark.sql(f"DROP VOLUME {catalog_name}.{schema_name}.{volume_name}")
+                print(f"Dropped Volume: {catalog_name}.{schema_name}.{volume_name}")
+            except:
+                print(
+                    f"Unable to drop Volume: {catalog_name}.{schema_name}.{volume_name}"
+                )
+
+
+def drop_schemas(spark, dbutils, catalog_name, schema_names):
+    schema_names = [i for i in schema_names if i != "information_schema"]
+    if len(schema_names) > 0:
+        print(f"Found Schemas In Catalog {catalog_name}: {schema_names}")
+
+        for schema_name in schema_names:
+            try:
+                print(f"Checking for Tables in Schema: {catalog_name}.{schema_name}")
+                table_names = [
+                    x.tableName
+                    for x in spark.sql(
+                        f"SHOW TABLES IN {catalog_name}.{schema_name}"
+                    ).collect()
+                ]
+            except:
+                print(f"Failed to list Tables in Schema: {catalog_name}.{schema_name}")
+            drop_tables(spark, catalog_name, schema_name, table_names)
+
+            try:
+                print(f"Checking for Volumes in Schema: {catalog_name}.{schema_name}")
+                volume_names = [
+                    x.volume_name
+                    for x in spark.sql(
+                        f"SHOW VOLUMES IN {catalog_name}.{schema_name}"
+                    ).collect()
+                ]
+            except:
+                print(f"Failed to list Volumes in Schema: {catalog_name}.{schema_name}")
+            drop_volumes(spark, dbutils, catalog_name, schema_name, volume_names)
+
+            try:
+                spark.sql(f"DROP SCHEMA {catalog_name}.{schema_name}")
+                print(f"Dropped Schema: {catalog_name}.{schema_name}")
+            except:
+                print(f"Unable to drop Schema: {catalog_name}.{schema_name}")
+
+
+def drop_catalog(spark, dbutils, catalog_name):
+    try:
+        print(f"Checking for Schemas in Catalog: {catalog_name}")
+        schema_names = [
+            x.databaseName
+            for x in spark.sql(f"SHOW SCHEMAS IN {catalog_name}").collect()
+        ]
+    except:
+        print(f"Failed to list Schemas in Catalog: {catalog_name}")
+
+    drop_schemas(spark, dbutils, catalog_name, schema_names)
+
+    try:
+        spark.sql(f"DROP CATALOG {catalog_name}")
+        print(f"Dropped Catalog: {catalog_name}")
+    except:
+        print(f"Unable to drop Catalog: {catalog_name}")
+
+
+def drop_catalogs(spark, dbutils, catalog_names):
+    if len(catalog_names) > 0:
+        for catalog_name in catalog_names:
+            print(f"Attempting to drop Catalog: {catalog_name}")
+            drop_catalog(spark, dbutils, catalog_name)
+
+
+def drop_external_locations(spark, external_location_names):
+    if len(external_location_names) > 0:
+        for external_location_name in external_location_names:
+            print(f"Attempting to drop External Location: {external_location_name}")
+            try:
+                spark.sql(f"DROP EXTERNAL LOCATION IF EXISTS {external_location_name}")
+                print(f"Dropped External Location: {external_location_name}")
+            except:
+                print(f"Unable to drop External Location: {external_location_name}")
+
+
+if __name__ == "__main__":
+    spark = get_sparksession()
+    dbutils = get_dbutils(spark)
+
+    catalog_names = ["df_test_catalog"]
+    external_location_names = ["df_test_location"]
+
+    drop_catalogs(spark, dbutils, catalog_names)
+    drop_external_locations(spark, external_location_names)
