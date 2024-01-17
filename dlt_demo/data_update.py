1bb9f1dd1b78fba7095e72e4a0e3c3268cdbe386
David Finch <david.finch@databricks.com>
2024-01-17 01:14:31 -0500

    .

---
+++ dlt_demo/data_update.py
@@ -0,0 +1,53 @@
+user_id = spark.sql('select current_user() as user').collect()[0]['user']
+storage_account = "oneenvadls"
+storage_container = "databricks-poc"
+
+pii_source = f"file:/Workspace/Repos/{user_id}/workshop/fixtures/sample_data/updates/CustomerPIIData_update.csv"
+pii_volume_destination = f"/Volumes/umpqua_poc_dev/bronze_data/bronze_volume/sample_data/pipe_delim_files/customerpiidata"
+pii_landing_destination = f"abfss://{storage_container}@{storage_account}.dfs.core.windows.net/umpqua_poc/landing_zone/customerpiidata"
+limits_source = f"file:/Workspace/Repos/{user_id}/workshop/fixtures/sample_data/updates/CustomerGTLimits_update.csv"
+limits_volume_destination = f"/Volumes/umpqua_poc_dev/bronze_data/bronze_volume/sample_data/pipe_delim_files/customergtlimits"
+limits_landing_destination = f"abfss://{storage_container}@{storage_account}.dfs.core.windows.net/umpqua_poc/landing_zone/customergtlimits"
+destination = "/Volumes/umpqua_poc_dev/bronze_data/bronze_volume/sample_data/csv_files"
+
+pii_df = (
+    spark.read.format("csv")
+    .option("delimiter", ",")
+    .option("header", "True")
+    .load(pii_source)
+)
+
+pii_df.write.csv(
+    path=pii_volume_destination,
+    sep="||",
+    header=True,
+    mode="overwrite",
+)
+
+pii_df.write.csv(
+    path=pii_landing_destination,
+    sep="||",
+    header=True,
+    mode="overwrite",
+)
+
+limits_df = (
+    spark.read.format("csv")
+    .option("delimiter", ",")
+    .option("header", "True")
+    .load(limits_source)
+)
+
+limits_df.write.csv(
+    path=pii_volume_destination,
+    sep="||",
+    header=True,
+    mode="overwrite",
+)
+
+limits_df.write.csv(
+    path=pii_landing_destination,
+    sep="||",
+    header=True,
+    mode="overwrite",
+)
