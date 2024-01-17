3a4924ca8fee7904eb7650863a4b50ce6f6496f5
David Finch <david.finch@databricks.com>
2024-01-17 03:39:17 -0500

    .

--- dlt_demo/dlt_demo_pipeline.py
+++ dlt_demo/dlt_demo_pipeline.py
@@ -96,40 +96,19 @@ def customerpiidata_clean_dlt():
     },
 )
 def corporate_customer_data_dlt():
-    pii_df = dlt.read_stream("customerpiidata_clean").filter(col("is_company") == 1)
-    limit_df = dlt.read_stream("customergtlimits")
-    columns = [
-        "customergtlimits.customer_id",
-        "customergtlimits.customer_name",
-        "customergtlimits.token_required_limit",
-        "customergtlimits.dual_approval_limit",
-        "customergtlimits.limit_per_transaction",
-        "customergtlimits.limit_per_day",
-        "customergtlimits.limit_per_acct_per_day",
-        "customergtlimits.limit_per_month",
-        "customergtlimits.aggregate_ach_limit_per_day",
-        "customerpiidata_clean.group_id",
-        "customerpiidata_clean.group_name",
-        "customerpiidata_clean.tax_id",
-        "customerpiidata_clean.is_company",
-        "customerpiidata_clean.is_treasury",
-        "customerpiidata_clean.primary_cif",
-        "customerpiidata_clean.service_charge_plan_id",
-        "customerpiidata_clean.plan_name",
-        "customerpiidata_clean.charge_account",
-        "customerpiidata_clean.create_date",
-        "customerpiidata_clean.street_address1",
-        "customerpiidata_clean.street_address2",
-        "customerpiidata_clean.city",
-        "customerpiidata_clean.state",
-        "customerpiidata_clean.postal_code",
-        "customerpiidata_clean.province",
-        "customerpiidata_clean.is_international",
-        "customerpiidata_clean.iso_code_a3"
-    ]
-    return pii_df.join(
-        limit_df, pii_df.customer_id == limit_df.customer_id, "left"
-    ).select(columns)
+    return dlt.read_stream("customerpiidata_clean").filter(col("is_company") == 1)
+
+
+@dlt.table(
+    name="consumer_customer_data",
+    comment="Clean & combined data about consumer customers only",
+    table_properties={
+        "quality": "gold",
+        "pipelines.autoOptimize.managed": "true",
+    },
+)
+def corporate_customer_data_dlt():
+    return dlt.read_stream("customerpiidata_clean").filter(col("is_company") == 0)
 
 
 # COMMAND ----------
